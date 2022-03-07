fd0656333ae9e023eabac16b31b2bf60aee24cdf
jlrua <jlrua@LAPTOP-2E1IO961.home>
2022-03-07 20:43:26 +0100

    Documenta el código que has obtenido como resultado de todas las actividades anteriores de forma que quede claro lo que realiza: qué función realiza cada clase y/o método, cada uno de los parámetros necesarios, el autor, la versión, si se ha quedado algún método obsoleto y cualquier otra cuestión que consideres relevante. Una vez documentado el código genera el fichero javaDoc. Lo realizarás tanto en Eclipse como en Netbeans. Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio. Deberás presentar capturas de pantalla de todo lo que vas haciendo y de los apartados más relevantes del documento JavaDoc donde aparecen reflejados.

--- EDtareagithub/src/notas/califica.java
+++ EDtareagithub/src/notas/califica.java
@@ -8,17 +8,23 @@ public class califica {
 	Scanner reader = new Scanner(System.in);// Para pedir números por teclado
 	int n = 0;
 	String c = "";
+	//nos pide introducir un numero
 	System.out.print("Introduzca una nota: ");
 	n = reader.nextInt();
 	
+	//si la nota dada es entre 0 y menos de 5 nos aparece un texto que confirma que la nota esta suspensa
 	if (n >= 0 && n < 5)
 	c = "Suspenso";
+	//si la nota dada es entre 5 y menor de 7 nos aparece un texto que confirma que la nota esta aprobada
 	else if (n >= 5 && n < 7)
 	c = "Aprobado";
+	//si la nota dada es entre 7 y menor de 9 nos aparece un texto que nos dice que la nota es notable
 	else if (n >= 7 && n <9)
 	c = "Notable";
+	//si la nota dada es entre 9 y 10 nos aparece un texto que nos dice que la nota es un sobresaliente
 	else if (n >= 9 && n <= 10)
 	c = "Sobresaliente";
+	//si la nota dada que damos no se trata de numeros, o estos son inferior a 0 o superior a 10 la nota no es correcta
 	else
 	c = "La nota introducida no es correcta";
 	
